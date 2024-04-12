218935b2e86fd1122f373075b9bf80316e394d4b
feryndka <feryandika151@gmail.com>
2024-04-13 00:07:05 +0700

    build:skeleton

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,60 @@
+"use client";
 import BlogCard from "@/components/BlogCard";
+import { useState, useEffect } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card } from "@/components/ui/card";
 
-const Home = async () => {
-  const res = await fetch("https://fakestoreapi.com/products");
-  const data = await res.json();
+const Home = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const result = await res.json();
+      setData(result);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <Loading />
+        <Loading />
+        <Loading />
+        <Loading />
+        <Loading />
+        <Loading />
+      </>
+    );
+  }
 
   return (
     <div>
       {data.map((item, index) => {
-        return (
-          <BlogCard
-            key={index}
-            thumbnail={item.image}
-            title={item.title}
-            desc={item.description}
-            price={item.price}
-          />
-        );
+        return <BlogCard key={index} products={item} />;
       })}
     </div>
   );
 };
 
 export default Home;
+
+function Loading() {
+  return (
+    <Card className="flex flex-col gap-2 my-4 items-center md:items-start md:flex-row">
+      <div className="w-44 h-32 flex items-center justify-center m-2">
+        <Skeleton className="h-full w-full" />
+      </div>
+      <div className="flex flex-col items-center justify-between w-[100%] h-32 md:items-start py-4">
+        <div className="w-full flex flex-col items-center md:items-start">
+          <Skeleton className="h-4 w-[50%] mb-2" />
+          <Skeleton className="h-4 w-[80%]" />
+        </div>
+        <Skeleton className="h-8 w-14"></Skeleton>
+      </div>
+    </Card>
+  );
+}
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Badge } from "@/components/ui/badge";
 import {
   Card,
@@ -9,31 +8,33 @@ import {
 } from "@/components/ui/card";
 import Image from "next/image";
 
-const BlogCard = ({ thumbnail, title, desc, price }) => {
+const BlogCard = ({ products }) => {
   return (
     <Card className="my-5 hover:shadow-md dark:hover:shadow-[0_0_10px_grey]">
-      <div className="md:flex items-center">
-        <div className="md:w-32 md:h-32 flex items-center justify-center">
-          <div className="w-32 h-full flex items-center m-2">
-            <Image
-              src={thumbnail}
-              width={500}
-              height={500}
-              alt="thumbnail"
-              className="w-32 h-32"
-            />
+        <div className="md:flex items-center">
+          <div className="md:w-32 md:h-32 flex items-center justify-center">
+            <div className="w-32 h-full flex items-center m-2">
+              <Image
+                src={products.image}
+                width={500}
+                height={500}
+                alt="thumbnail"
+                className="w-32 h-32"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between flex-col items-center md:items-start text-center md:text-start">
+            <CardHeader>
+              <CardTitle>{products.title}</CardTitle>
+              <CardDescription>{products.description}</CardDescription>
+            </CardHeader>
+            <CardFooter>
+              <Badge variant="outline" className="px-4 py-2 text-sm">
+                ${products.price}
+              </Badge>
+            </CardFooter>
           </div>
         </div>
-        <div className="flex justify-between flex-col items-center md:items-start text-center md:text-start">
-          <CardHeader>
-            <CardTitle>{title}</CardTitle>
-            <CardDescription>{desc}</CardDescription>
-          </CardHeader>
-          <CardFooter>
-            <Badge variant="outline" className="px-4 py-2 text-sm">${price}</Badge>
-          </CardFooter>
-        </div>
-      </div>
     </Card>
   );
 };
